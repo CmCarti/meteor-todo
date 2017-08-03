c2c20afe34c91412e2e89be672a9e2ca59a708f0
Chad cartier <chadcartier@pcm.local>
2017-08-03 09:54:00 -0400

    Fixed syntax error in renderTasks() in App.jsx

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,17 +3,17 @@ import Task from './Task.jsx';
 
 export default class App extends Component {
     getTasks() {
-        return [
-            { _id: 1, text: 'This is task 1'},
-            { _id: 2, text: 'This is task 2'},
-            { _id: 3, text: 'This is task 3'},
+            return [
+        { _id: 1, text: 'This is task 1' },
+        { _id: 2, text: 'This is task 2' },
+        { _id: 3, text: 'This is task 3' },
         ];
     }
 
     renderTasks(){
-        return this.getTasks().map( (task) => {
+        return this.getTasks().map( (task) => (
             <Task key={task._id} task={task} />
-        });
+        ));
     }
 
     render() {
